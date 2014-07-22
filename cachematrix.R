1aa31aa072f697e464830e5344571552703c68fd
markmercer <mark.mercer@gmail.com>
2014-07-21 22:02:21 -0400

    Implemented the makeCacheMatrix and cacheSolve routines. Also added a .gitignore file for vim swaps.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## constructs a structure that can be used to hold
+## a matrix as well as the precomputed inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
+    xinv <- NULL
+    set <- function(y) {
+	x <<- y
+	xinv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(mean) xinv <<- mean
+    getinv <- function() xinv 
+    list(set = set, get = get,
+     setinv = setinv,
+     getinv = getinv)
 
 }
 
-
-## Write a short comment describing this function
-
+## Returns the invers of a given matrix. Uses the precomputed inverse 
+## of a matrix if it exists, otherwise calculates the inverse dynamically. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
